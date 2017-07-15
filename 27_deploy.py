b879421e25f18b39e8ab140344a630a80c4d261b
Brian Bloechle <bbloechle@cloudera.com>
2017-07-15 18:18:42 +0000

    Add partial version of 27_deploy.py and 27_deploy.scala - Include example of saving a transformer in Python and loading it in Scala

---
+++ 27_deploy.py
@@ -0,0 +1,81 @@
+# # Deploying Models and Pipelines in Production
+
+# In this module we show how save, load, and apply transformers, estimators, and pipelines.
+
+
+# ## TODO
+# * Create outline
+
+
+# ## Setup
+
+# Create a SparkSession:
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("deploy").master("local[2]").getOrCreate()
+
+# Read the enhanced ride data from HDFS:
+rides = spark.read.parquet("/duocar/joined")
+
+
+# ## Preprocess the data
+
+# Convert `distance` from integer to double.
+pre1 = rides.withColumn('distance_double', rides.distance.cast('double'))
+
+# Convert `star_rating` from integer to double.
+pre2 = pre1.withColumn('star_rating_double', rides.star_rating.cast('double'))
+
+# Rename preprocessed DataFrame:
+preprocessed = pre2
+
+
+# ## Transform the data
+
+# ### Binarize `star_rating`
+
+# Specify
+# [Binarizer](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.feature.Binarizer):
+from pyspark.ml.feature import Binarizer
+binarizer = Binarizer(inputCol='star_rating_double', outputCol='star_rating_binarized', threshold=3.5)
+
+# **Note:**  `Binarizer` requires the input column to be of type double.
+
+# Apply binarizer:
+binarized = binarizer.transform(preprocessed)
+
+# Verify transformation:
+binarized.select('star_rating', 'star_rating_binarized').show()
+
+# Null values get mapped to null values.
+binarized.filter(binarized.star_rating.isNull()).select('star_rating', 'star_rating_binarized').show()
+
+# Save the Binarizer to HDFS:
+!hdfs dfs -rm -r duocar/binarizer
+binarizer.save("duocar/binarizer")
+
+# **Note**: We are saving to our user directory in HDFS.
+# The Binarizer instance is stored in JSON format.
+# You can view it in Hue.
+
+
+# ## Integrate with Hive?
+# * Add column to existing Hive table?
+# * Add new Hive table?
+
+
+# ## Integrate with Kudu or HBase?
+
+
+# ## PMML?
+
+
+# ## Exercises
+
+
+# ## Cleanup
+
+# Stop the SparkSession:
+spark.stop()
+
+
+# ## References
---
+++ 27_deploy.scala
@@ -0,0 +1,48 @@
+/* # Deploying Models and Pipelines in Production */
+
+/* **Note**: // comments do not render Markdown. */
+
+/* In this module we demonstrate how to load transformers, estimators, and pipelines
+that were saved from Python into Scala. */
+
+
+/* ## Setup */
+
+/* In a Scala session, the SparkSession is created for us: */
+spark
+
+/* Read the enhanced ride data from HDFS: */
+val rides = spark.read.parquet("/duocar/joined_all/")
+
+/* **Note:** We are reading a different dataset (joined_all rather than joined). */
+
+
+/* ## Preprocess the data */
+
+/* Convert `star_rating` from integer to double: */
+val pre2 = rides.withColumn("star_rating_double", rides("star_rating").cast("double"))
+
+/* Rename the preprocessed DataFrame: */
+val preprocessed = pre2
+
+  
+/* ## Transform the data */
+
+/* ### Binarize star_rating */
+
+/* Read the
+[Binarizer](http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.ml.feature.Binarizer)
+instance from HDFS: */
+import org.apache.spark.ml.feature.Binarizer
+val binarizer = Binarizer.load("duocar/binarizer")
+
+/* Inspect the Binarizer instance: */
+binarizer.getInputCol
+binarizer.getOutputCol
+binarizer.getThreshold
+
+/* Apply the Binarizer: */
+val binarized = binarizer.transform(preprocessed)
+
+/* Verify transformation */
+binarized.groupBy("star_rating", "star_rating_binarized").count().orderBy("star_rating").show()
