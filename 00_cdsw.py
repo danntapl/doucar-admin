5f680c959dcaed8f8e10faa2df16458a8491ca03
Brian Bloechle <bbloechle@cloudera.com>
2017-07-18 13:07:17 +0000

    Update 00_cdsw.py - Move plotting example up - Move working with packages up - Replace avro with seaborn - Move LaTeX example up - Make minor edits

--- 00_cdsw.py
+++ 00_cdsw.py
@@ -3,11 +3,15 @@
 
 # ## Entering Code
 
-# Enter code as you normally would in a source file:
+# Enter code as you normally would in the shell or a script:
 
 print("Hello, CDSW!")
 
 2 + 2
+
+import random
+import matplotlib.pyplot as plt
+plt.plot([random.normalvariate(0,1) for i in xrange(1,1000)])
 
 
 # ## Accessing the Command Line
@@ -19,10 +23,41 @@ print("Hello, CDSW!")
 !ls -l
 
 
+# ## Working with Python Packages
+
+# **Important:** Packages are managed on a project-by-project basis.
+
+# Use `!pip list` to get a list of the currently installed packages:
+!pip list --format=columns
+
+# Search for an available Python package (seaborn in this case):
+!pip search seaborn
+
+# Install a new package. Note that this package is now installed for all
+# sessions associated with this project.
+!pip install seaborn
+
+# Show details about an installed package.  This returns nothing if the package
+# is not installed.
+!pip show seaborn
+
+# Use seaborn:
+import numpy as np
+import seaborn as sns
+x = np.random.normal(size=100)
+sns.distplot(x, rug=True)
+
+# Uninstall package. (Note: Include the '-y' option to avoid a prompt asking
+# you to confirm the delete.)
+!pip uninstall -y seaborn
+
+
 # ## Formatting Session Output
 
-# Comments in your file can include Markdown for presentation.  You'll see some 
-# examples here, and you can see the References section below to find out more.
+# Comments in your file can include
+# [Markdown](https://daringfireball.net/projects/markdown/syntax) for
+# presentation.  You'll see some examples here, and you can see the References
+# section below to find out more.
 
 # ### Headings
 
@@ -32,7 +67,7 @@ print("Hello, CDSW!")
 
 # ### Heading 3
 
-# ### Text Formats
+# ### Text
 
 # Plain text
 
@@ -42,12 +77,21 @@ print("Hello, CDSW!")
 
 # `Code text` (Note these are backtick quotes.)
 
+# ### Mathematical Text
+
+# Display an inline term like $\bar{x} = \frac{1}{n} \sum_{i=1}^{n} x_i$ using
+# a [LaTeX](https://en.wikibooks.org/wiki/LaTeX/Mathematics) expression
+# surrounded by dollar-sign characters.
+
+# A math expression can be displayed set apart by surrounding the LaTeX
+# shorthand with double dollar-signs, like so: $$f(x)=\frac{1}{1+e^{-x}}$$
+
 # ### Lists
 
 # Bulleted List
 # * Item 1
 #   * Item 1a
-#   * Item 2b
+#   * Item 1b
 # * Item 2
 # * Item 3
 
@@ -60,53 +104,12 @@ print("Hello, CDSW!")
 
 # Link to [Cloudera](http://www.cloudera.com)
 
-
-# ### Math formulas
-
-# Display an inline term like $\displaystyle\sum_{i=1}^{N} t_i$ using
-# a TeX expression surrounded by dollar-sign characters.
-
-
-# A math expression can be displayed set apart by surrounding the TeX 
-# shorthand with double dollar-signs,
-# like so: $$f(x)=\frac{1}{1+e^{-x}}$$
-
-
-# ### Displaying images
+# ### Images
 
 # Displaying a stored image file:
 
 from IPython.display import Image
-
 Image("spark.png")
-
-# Creating a simple plot:
-
-import matplotlib.pyplot as plt
-import random
-plt.plot([random.normalvariate(0,1) for i in xrange(1,1000)])
-
-
-# ## Working with Python Packages
-
-# **Important:** Packages are managed on a project-by-project basis.
-
-# Use `!pip list` to get a list of the currently installed packages:
-!pip list --format=columns
-
-# Search for an available Python package (avro in this case).
-!pip search avro
-
-# Install a new package. Note that this is now installed for all sessions in 
-# this project.
-!pip install avro
-
-# Show details about an installed package.  This returns nothing if the package is not installed.
-!pip show avro
-
-# Uninstall package. (Note: Include the '-y' option to avoid a prompt asking you to confirm 
-# the delete.)
-!pip uninstall -y avro
 
 # Dummy Python command to get Linux commands to print.  
 # See [DSE-2112](https://jira.cloudera.com/browse/DSE-2112).
@@ -115,9 +118,8 @@ print("Fin")
 
 # ## References
 
-# Workbench: [Cloudera Data Science Workbench Documentation](https://www.cloudera.com/documentation/data-science-workbench/latest.html)
+# [Cloudera Data Science Workbench](https://www.cloudera.com/documentation/data-science-workbench/latest.html)
 
-# Markdown syntax: [Daring Fireball Markdown](https://daringfireball.net/projects/markdown/)
+# [Markdown](https://daringfireball.net/projects/markdown/)
 
-# LaTeX for mathematical expressions: [LaTeX/Mathematics](https://en.wikibooks.org/wiki/LaTeX/Mathematics)
-
+# [LaTeX](https://en.wikibooks.org/wiki/LaTeX/Mathematics)
