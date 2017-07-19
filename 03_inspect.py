b316d5d06dad270a9414c8fc6358ad46bbf8dfc8
Brian Bloechle <bbloechle@cloudera.com>
2017-07-19 19:10:53 +0000

    Update 03_inspect.py - Fix file paths - Add additional examples - Update commentary - Add exercises - Add links and references - Replace single quotes with double quotes

--- 03_inspect.py
+++ 03_inspect.py
@@ -12,52 +12,68 @@
 #   * Inspecting a datetime variable
 
 
-# ## Connect to the Spark cluster
+# ## Create a SparkSession
 
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.master('local').appName('inspect').getOrCreate()
+spark = SparkSession.builder.master("local").appName("inspect").getOrCreate()
 
-# ## Load the rider data from HDFS into a Spark DataFrame ##
+# **Note:** We are using Spark in local mode to speed things up on this small
+# dataset.
 
-riders = spark.read.csv('duocar/riders_fargo.txt', sep='\t', header=True, inferSchema=True)
 
-# **Developer Note:** We are using a local Spark instance to speed things up on this small dataset.
+# ## Load the rider data from HDFS into a Spark DataFrame
+
+riders = spark.read.csv("/duocar/raw/riders/", header=True, inferSchema=True)
+
+# **Note:** The default filesystem is HDFS and Spark will attempt to read all
+# files in the HDFS directory.
 
 
 # ## Examining the schema
 
-# Use the `printSchema` method to examine the schema.
+# Use the `printSchema` method of the
+# [DataFrame](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.DataFrame)
+# class to examine the schema:
+
 riders.printSchema()
 
-# Use the `schema`, `dtypes`, and `columns` attributes to view the schema in other ways.
-riders.schema
+# Use the `dtypes`, `columns`, and `schema` attributes to view the schema in other ways:
+
 riders.dtypes
 riders.columns
+riders.schema
+
+# **Note:** The `schema` attribute provides a programmatic version of the schema.
 
 
 # ## Viewing some data
 
-# Use the `show` method to get a SQL-like display of the data.
+# Use the `show` method to get a SQL-like display of the data:
+
 riders.show(5)
 
-# Use the `head` or `take` method to get a display of the `Row` objects that is sometimes easier to read.
+# Use the `head` or `take` method to get a display of the `Row` objects that is sometimes easier to read:
+
 riders.head(5)
 
 # **Notes:** 
+# * `id` and `home_block` are long integers rather than key strings.
 # * `birth_date` and `start_date` are Timestamps rather than Dates.
-# * `ethnicity` appears to have null (missing) values.
-# * `home_block` in as a long integer rather than a string.
+# * `ethnicity`, `work_lat`, and `work_lon` appear to have null (missing) values.
+# * `student` is a boolean variable inefficiently encoded as an integer.
 
 
 # ## Computing the number of rows (observations)
 
-# Use the `count` method to compute the number of rows.
+# Use the `count` method to compute the number of rows:
+
 riders.count()
 
 
 # ## Computing summary statistics
 
-# Use the `describe` method to compute summary statistics for numeric and string columns.
+# Use the `describe` method to compute summary statistics for numeric and string columns:
+
 riders.describe().show()
 
 # **Notes:** 
@@ -65,68 +81,107 @@ riders.describe().show()
 # * `sex` and `ethnicity` seem to have some missing values.
 
 
-# ## Inspecting a column (variable) ##
+# ## Inspecting a column (variable)
 
-# We often want to inspect one variable at a time.
+# We often want to inspect one or a few variables at a time.
+
 
 # ### Inspect a key variable ###
 
-# The variable `id` should be a unique value.  Let us confirm this.
+# The variable `id` should be a unique value.  Let us confirm this.  Use the
+# `select` method to select the `id` column:
 
-riders.describe('id').show()
+riders.select("id").describe().show()
+
+# Alternatively, pass the column name to the `describe` method:
+
+riders.describe("id").show()
 
 # Use the `countDistinct` function to determine the number of distinct values:
+
 from pyspark.sql.functions import count, countDistinct
-riders.select(count('id'), countDistinct('id')).show()
+riders.select(count("id"), countDistinct("id")).show()
+
 # **Note:** This can take quite a bit of time on large DataFrames.
+
 
 # ### Inspect a categorical variable
 
-# The variable `sex` is a categorical variable.
-riders.select(count('*'), count('sex'), countDistinct('sex')).show()
+# The variable `sex` is a categorical variable.  Let us examine it more carefully:
+
+riders.select(count("*"), count("sex"), countDistinct("sex")).show()
 
 # Use the `select` method to select a subset of columns:
-riders.select('sex').show(5)
 
-# Use the `distinct` method to determine the uniques values of `sex`:
-riders.select('sex').distinct().count()
+riders.select("sex").show(5)
+
+# Use the `distinct` method to determine the unique values of `sex`:
+
+riders.select("sex").distinct().count()
 
 # **Developer Note:** See https://jira.cloudera.com/browse/DSE-1159 for the print issue.
 
-riders.select('sex').distinct().show()
+riders.select("sex").distinct().show()
 
-# An alternative approach is use an aggregation.
+# An alternative approach is to use an aggregation:
 
-riders.groupby('sex').count().show()
+riders.groupby("sex").count().show()
 
 # **Note:** `sex` contains null (missing) values that we must deal with.
 
+
 # ### Inspecting a numerical variable
 
-riders.select('home_lat', 'home_lon').show(5)
-
-riders.select('home_lat', 'home_lon').describe().show(5)
+riders.select("home_lat", "home_lon").show(5)
+riders.select("home_lat", "home_lon").describe().show(5)
 
 # **Notes:**
 # * No missing values
 # * No extreme values (tight distribution about [Fargo, North Dakota](https://en.wikipedia.org/wiki/Fargo%2C_North_Dakota))
 
+# Use the `approxQuantile` to get customized (approximate) quantiles:
+
+riders.approxQuantile("home_lat", probabilities=[0.0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1.0], relativeError=0.1)
+
+# **Note:** This method returns a Python list.  Spark 2.2 adds support for multiple columns.
+
+
 # ### Inspecting a datetime variable
 
-# Let us inspect `birth_date` and `start_date`, which are both Timestamp variables.
+# Let us inspect `birth_date` and `start_date`, which are both Timestamp
+# variables:
 
-riders.select('birth_date', 'start_date').show(5)
+riders.select("birth_date", "start_date").show(5, truncate=False)
+riders.select("birth_date", "start_date").head(5)
 
-riders.select('birth_date', 'start_date').head(5)
+# Note that the original data was in Date format, but Spark read the data in
+# Timestamp format.  We will probably want to fix this.
 
-# Note that the original data was in Date format, but Spark read the data in Timestamp format.
-# We will probably want to fix this.
+# Note that the `describe` method does not work with Date or Timestamp variables:
 
-# Note that the `describe` method does not work with Date or Timestamp variables.
-riders.select('birth_date', 'start_date').describe().show(5)
+riders.select("birth_date", "start_date").describe().show(5)
+
+
+# ## Exercises
+
+# Read the raw driver data from HDFS into a Spark DataFrame.
+
+# Inspect the driver DataFrame.  Are the data types for each column appropriate?
+
+# Inspect various columns of the driver DataFrame.  Are there any issues with the data?
 
 
 # ## Cleanup
 
-# Stop the SparkSession.
+# Stop the SparkSession:
+
 spark.stop()
+
+
+# ## References
+
+# [Spark SQL, DataFrames, and Datasets Guide](http://spark.apache.org/docs/latest/sql-programming-guide.html)
+
+# [DataFrame class](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.DataFrame)
+
+# [spark.sql.types module](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#module-pyspark.sql.types)
