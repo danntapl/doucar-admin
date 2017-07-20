d35731abe8c05ea7aa758de3d7af7a44cd5873b2
Ian Cook <icook@cloudera.com>
2017-07-20 20:04:14 +0000

    add 02_read.R

---
+++ 02_read.R
@@ -0,0 +1,155 @@
+# # Reading and Writing Data
+
+# ## Setup
+
+library(sparklyr)
+spark <- spark_connect(master = "local", app_name = "read")
+
+
+# ## Working with delimited text files
+
+# The rider data is a comma-delimited text file
+# Use the `spark_read_csv()` function to read it into a Spark DataFrame
+
+# ### Infer the schema
+
+# With only the required arguments:
+
+riders <- spark_read_csv(
+  sc = spark,
+  name = "riders",
+  path = "/duocar/raw/riders/"
+)
+
+# Showing some more arguments, which are set to the default values:
+
+riders <- spark_read_csv(
+  sc = spark,
+  name = "riders",
+  path = "/duocar/raw/riders/",
+  header = TRUE,
+  infer_schema = TRUE,
+  delimiter = ","
+)
+
+# Show the schema and first few rows of data:
+riders
+
+# The returned object (`riders` in this example) is a `tbl_spark`.
+
+class(riders)
+
+# This represents a remote Spark DataFrame. It is not loaded in R's memory.
+# Notice that only the first few rows of data are shown.
+
+
+# ### Manually specify the schema
+
+riders2 <- spark_read_csv(
+  sc = spark,
+  name = "riders",
+  path = "/duocar/raw/riders/",
+  infer_schema = FALSE,
+  columns = c(
+    id = "character",
+    birth_date = "character",
+    start_date = "character",
+    first_name = "character",
+    last_name = "character",
+    sex = "character",
+    ethnicity = "character",
+    student = "integer",
+    home_block = "character",
+    home_lat = "numeric",
+    home_lon = "numeric",
+    work_lat = "numeric",
+    work_lon = "numeric"
+  )
+)
+
+riders2
+
+
+# Write the file to a tab-delimited file:
+
+system("hdfs dfs -rm -r -skipTrash practice")
+
+system("hdfs dfs -mkdir practice")
+
+spark_write_csv(
+  riders,
+  path = "practice/riders_tsv",
+  delimiter = "\t"
+)
+
+system("hdfs dfs -ls practice/riders_tsv")
+
+system("hdfs dfs -cat practice/riders_tsv/* | head -n 5")
+
+
+# ## Working with Parquet files
+
+spark_write_parquet(
+  riders,
+  path = "practice/riders_parquet"
+)
+
+system("hdfs dfs -ls practice/riders_parquet")
+
+# Note that the schema is stored with the data
+
+riders_parquet <- spark_read_parquet(
+  sc = spark,
+  name = "riders_parquet",
+  path = "practice/riders_parquet"
+)
+riders_parquet
+
+
+# ## Working with Hive Tables
+
+# Use the `DBI::dbGetQuery()` function to run SQL queries:
+
+library(DBI)
+
+dbGetQuery(spark, "SHOW DATABASES")
+
+dbGetQuery(spark, "SHOW TABLES")
+
+dbGetQuery(spark, "DESCRIBE airlines")
+
+dbGetQuery(spark, "SELECT * from airlines limit 10")
+
+
+# `dbGetQuery()` returns the query result to R as a data frame
+
+airlines <- dbGetQuery(spark, "SELECT * FROM airlines")
+
+class(airlines)
+
+airlines
+
+# Only use `dbGetQuery()` when the query result will be small enough to fit in memory in your R session.
+
+
+# You can also return the result of a SQL query as a `tbl_spark`.
+# To do this, use `tbl(spark, sql(...))`.
+# This will *not* return the full query result; it will only display the first few rows.
+
+flights <- tbl(spark, sql("SELECT * FROM flights"))
+
+class(flights)
+
+flights
+
+
+# ## Cleanup
+
+# Remove practice directory from HDFS:
+
+system("hdfs dfs -rm -r -skipTrash practice/riders_tsv")
+system("hdfs dfs -rm -r -skipTrash practive/riders_parquet")
+
+# Stop the `SparkSession`:
+
+spark_disconnect(spark)
