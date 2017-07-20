9b654712dad42b31f5d45c85d04c2cc8fbe58290
Brian Bloechle <bbloechle@cloudera.com>
2017-07-20 23:08:15 +0000

    Update 05b_complex_types.py - Fix file paths - Replace single quotes with double quotes - Major revisions to examples and commentary - Add exercises - Add links references

--- 05b_complex_types.py
+++ 05b_complex_types.py
@@ -1,5 +1,7 @@
 # # Complex Types
 
+# In this module we demonstrate complex column types in Spark SQL.
+
 # * Arrays
 # * Maps
 # * Structs
@@ -9,94 +11,125 @@
 
 # Create a SparkSession:
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName('complex_types').master('local').getOrCreate()
+spark = SparkSession.builder.master("local").appName("complex_types").getOrCreate()
 
-# Load ride data from HDFS:
-rides = spark.read.csv('duocar/rides_fargo.txt', sep='\t', header=True, inferSchema=True)
+# Load raw ride data from HDFS:
+rides = spark.read.csv("/duocar/raw/rides/", header=True, inferSchema=True)
 
-# Load driver data from HDFS:
-drivers = spark.read.csv('duocar/drivers_fargo.txt', sep='\t', header=True, inferSchema=True)
+# Load raw driver data from HDFS:
+drivers = spark.read.csv("/duocar/raw/drivers/", header=True, inferSchema=True)
 
-# Load rider data from HDFS:
-riders = spark.read.csv('duocar/riders_fargo.txt', sep='\t', header=True, inferSchema=True)
+# Load raw rider data from HDFS:
+riders = spark.read.csv("/duocar/raw/riders/", header=True, inferSchema=True)
 
 
 # ## Arrays
 
-# Use the [array](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.array) function to create an array from multiple columns:
+# Use the
+# [array](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.array)
+# function to create an array from multiple columns:
 from pyspark.sql.functions import array
-origin_array = rides.select('origin_lat', 'origin_lon', array('origin_lat', 'origin_lon').alias('origin'))
-origin_array.head(5)
+drivers_array = drivers.select("vehicle_make", "vehicle_model",\
+    array("vehicle_make", "vehicle_model").alias("vehicle_array"))
+drivers_array.printSchema()
+drivers_array.show(5, truncate=False)
 
-# **Note:** `origin` is a Python list.
+# **Note:** `vehicle_array` is a Python list.
 
-# Use the `size` function to get the length of the array:
+# Use the
+# [size](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.size)
+# function to get the length of the array:
 from pyspark.sql.functions import size
-origin_array.select('origin', size('origin')).head(5)
+drivers_array.select("vehicle_array", size("vehicle_array")).show(5, False)
 
-# Access the array elements using index syntax:
-
-origin_array.select('origin', origin_array.origin[0]).head(5)
+# Use index notation to access elements of the array:
+drivers_array.select("vehicle_array", drivers_array.vehicle_array[0]).show(5, False)
 
 # **Note:** Some equivalent alternatives to the previous expression:
 
-origin_array.select(origin_array['origin'][0]).head()
+drivers_array.select("vehicle_array", drivers_array["vehicle_array"][0]).show(1, False)
 
 from pyspark.sql.functions import col
-origin_array.select(col('origin')[0]).head()
+drivers_array.select("vehicle_array", col("vehicle_array")[0]).show(1, False)
 
 from pyspark.sql.functions import column
-origin_array.select(column('origin')[0]).head()
+drivers_array.select("vehicle_array", column("vehicle_array")[0]).show(1, False)
 
 from pyspark.sql.functions import expr
-origin_array.select(expr('origin[0]')).head()
+drivers_array.select("vehicle_array", expr("vehicle_array[0]")).show(1, False)
 
-origin_array.selectExpr('origin[0]').head()
+drivers_array.selectExpr("vehicle_array", "vehicle_array[0]").show(1, False)
 
-# Sort an array
+# Use the
+# [sort_array](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.sort_array)
+# function to sort the array:
+from pyspark.sql.functions import sort_array
+drivers_array.select("vehicle_array", sort_array("vehicle_array", asc=True)).show(5, False)
 
-# TDB
+# Use the `array_contains` function to search the array:
+from pyspark.sql.functions import array_contains
+drivers_array.select("vehicle_array", array_contains("vehicle_array", "Subaru")).show(5, False)
 
-# Search an array
-
-# TBD
+# Use the `explode` and `posexplode` functions to explode the array:
+from pyspark.sql.functions import explode, posexplode
+drivers_array.select("vehicle_array", explode("vehicle_array")).show(5, False)
+drivers_array.select("vehicle_array", posexplode("vehicle_array")).show(5, False)
 
 
 # ## Maps
 
-# Use the [create_map](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.create_map) function to create a map:
-from pyspark.sql.functions import create_map
-silly_map = drivers.select(create_map('id', 'last_name').alias('name_map'))
-silly_map.head(5)
+# Use the
+# [create_map](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.create_map)
+# function to create a map:
+from pyspark.sql.functions import lit, create_map
+drivers_map = drivers.select("vehicle_make", "vehicle_model",\
+    create_map(lit("make"), "vehicle_make", lit("model"), "vehicle_model").alias("vehicle_map"))
+drivers_map.printSchema()
+drivers_map.show(5, False)
 
-# **Question:** Can we find a more interesting example?
+# Use the `size` function to get the length of the map:
+drivers_map.select("vehicle_map", size("vehicle_map")).show(5, False)
 
-# Access a value by key
+# Use dot notation to access the value by key:
+drivers_map.select("vehicle_map", drivers_map.vehicle_map.make).show(5, False)
 
-# TBD
+# Use the `explode` and `posexplode` functions to explode the map:
+drivers_map.select("vehicle_map", explode("vehicle_map")).show(5, False)
+drivers_map.select("vehicle_map", posexplode("vehicle_map")).show(5, False)
 
 
 # ## Structs
 
-# Use the `struct` function to create a struct:
+# Use the
+# [struct](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.struct)
+# function to create a struct:
 from pyspark.sql.functions import struct
-vehicle_struct = drivers.select(struct('vehicle_make', 'vehicle_model', 'vehicle_year').alias('vehicle'))
-vehicle_struct.head(5)
+drivers_struct = drivers.select("vehicle_make", "vehicle_model",\
+    struct(drivers.vehicle_make.alias("make"),\
+    drivers.vehicle_model.alias("model")).alias("vehicle_struct"))
+drivers_struct.printSchema()
+drivers_struct.show(5, False)
 
 # **Note:** The struct is a `Row` object (embedded in a `Row` object).
-
-# **Question:** Is there a way to rename 'vehicle_make' to 'make' as part of the struct creation process?
-drivers.select(struct(drivers.vehicle_make.alias('make'),\
-                      drivers.vehicle_model.alias('model'),\
-                      drivers.vehicle_year.alias('year')).alias('vehicle')).head(5)
-
+drivers_struct.head(5)
 
 # Use dot notation to access struct items:
-vehicle_struct.select(vehicle_struct.vehicle.vehicle_make.alias('vehicle_make')).head(5)
+drivers_struct.select("vehicle_struct", col("vehicle_struct").make.alias("vehicle_make")).show(5, False)
+
+# **Note:** Using `col` is a bit more concise in this case.
 
 # Use the `to_json` function to convert the struct to a JSON string:
 from pyspark.sql.functions import to_json
-vehicle_struct.select(to_json('vehicle')).head(5)
+drivers_struct.select("vehicle_struct", to_json("vehicle_struct")).show(5, False)
+
+
+# ## Exercises
+
+# Create an array called **home_array** that includes driver's home latitude and longitude.
+
+# Create a map called **name_map** that includes the driver's first and last name.
+
+# Create a struct called **name_struct** that includes the driver's first and last name.
 
 
 # ## Cleanup
@@ -105,8 +138,11 @@ vehicle_struct.select(to_json('vehicle')).head(5)
 spark.stop()
 
 
-# ## TODO
-# * Complete array examples
-# * Complete map examples
-# * Complete struct examples
-# * Generate exercises
+# ## References
+
+# [pyspark.sql.functions.array](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.array)
+
+# [pyspark.sql.functions.create_map](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.create_map)
+
+# [pyspark.sql.functions.struct](http://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.functions.struct)
+
