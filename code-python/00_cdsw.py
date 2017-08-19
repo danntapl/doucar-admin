b0d022f3cf2885f973d7a14d4626eb99a6bebb81
Brian Bloechle <bbloechle@cloudera.com>
2017-08-19 12:08:49 +0000

    Make minor revisions to 00_cdsw.py - Include a note about using the arrow keys to navigate the command history

--- code-python/00_cdsw.py
+++ code-python/00_cdsw.py
@@ -25,7 +25,7 @@ sns.pairplot?
 # Use the following IPython help syntax to inspect the source code:
 #sns.pairplot??
 
-# **Developer Note:** The script does not run (using `Run All`) correctly when
+# **Developer Note:** The script does not run correctly (using `Run All`) when
 # this command is uncommented.
 
 
@@ -50,29 +50,33 @@ sns.pairplot?
 # Search for an available Python package (folium in this case):
 !pip search folium
 
-# Install a new package. Note that this package is now installed for all
-# sessions associated with this project.
+# Install a new package.
 !pip install folium
 
-# Show details about an installed package.  This returns nothing if the package
-# is not installed.
+# **Note:** This package is now installed for all sessions associated with this
+# project.
+
+# Show details about an installed package:
 !pip show folium
+
+# **Note:**  This returns nothing if the package is not installed.
 
 # Use folium:
 import folium
 folium.Map(location=[46.8772222, -96.7894444])
 
-# Uninstall package. (Note: Include the '-y' option to avoid a prompt asking
-# you to confirm the delete.)
+# Uninstall package:
 !pip uninstall -y folium
+
+# **Note:** Include the `-y` option to avoid an unseen prompt to confirm the uninstall.
 
 
 # ## Formatting Session Output
 
 # Comments in your file can include
 # [Markdown](https://daringfireball.net/projects/markdown/syntax) for
-# presentation.  You'll see some examples here, and you can see the References
-# section below to find out more.
+# presentation.  You will see some examples here, and you can see the
+# References section below to find out more.
 
 # ### Headings
 
@@ -125,22 +129,25 @@ folium.Map(location=[46.8772222, -96.7894444])
 from IPython.display import Image
 Image("resources/spark.png")
 
-# **Note:** The image path is relative to `/home/cdsw/` regardless of script location.
+# **Note:** The image path is relative to `/home/cdsw/` regardless of script
+# location.
 
 
 # ## Exercises
 
-# (1) Experiment with the CDSW command prompt.  Note the following:
+# (1) Experiment with the CDSW command prompt.  Run some commands and note the
+# following:
 # * Tab completion is available
 # * Some GNU Readline Library commands such as `C-f`, `C-b`, and `C-d` are
-# available.
+# available
+# * You can use the up and down arrows to navigate the command history
 
 # (2) Experiment with the CDSW editor.  Create a new file.  Enter Python code
 # and Markdown text.  Run the file line by line (or chunk by chunk).  Clear the
 # console log and run the entire file.
 
 # (3) Open a terminal window.  Type `env | grep PYTHON` to explore the Python
-# environmental variables
+# environmental variables.
 
 
 # ## References
