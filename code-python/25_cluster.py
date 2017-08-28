a5face6088686a424ce440ee781f2d2690e063ba
Brian Bloechle <bbloechle@cloudera.com>
2017-08-28 02:24:04 +0000

    Checkpoint 25_cluster.py and 25_lda.py

--- code-python/25_cluster.py
+++ code-python/25_cluster.py
@@ -1,15 +1,23 @@
-# # Clustering
+# # Building and evaluating clustering models
 
-# In this module we will use the clustering functionality is Spark MLlib to look for structure in the ride data.
+# In this module we use the clustering functionality is Spark MLlib to look for
+# structure in the ride data.
 
 
 # ## Setup
 
+# Install the folium package for map plotting:
+!pip install folium
+
+# Import useful packages, modules, classes, and functions:
+from __future__ import print_function
+import folium
+
 # Create a SparkSession:
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName('cluster').master('local').getOrCreate()
+spark = SparkSession.builder.master("local").appName("cluster").getOrCreate()
 
-# Load enhanced ride data from HDFS:
+# Load the enhanced ride data from HDFS:
 rides = spark.read.parquet('/duocar/joined/')
 
 
@@ -17,6 +25,9 @@ rides = spark.read.parquet('/duocar/joined/')
 
 # Remove the canceled rides:
 rides_filtered = rides.filter(rides.cancelled == 0)
+
+
+# ## Extract, transform, and select the features
 
 # Select origin and destination coordinates:
 rides_selected = rides_filtered.select('origin_lat', 'origin_lon', 'dest_lat', 'dest_lon')
@@ -31,25 +42,32 @@ from pyspark.ml.feature import StandardScaler
 ss = StandardScaler(inputCol='features', outputCol='features_scaled', withMean=False, withStd=True)
 rides_standardized = ss.fit(rides_assembled).transform(rides_assembled)
 
-# **Note:** Given that latitude and longitude are already in the same scale, standardization may be superfluous in this case.
+# **Note:** Given that latitude and longitude are in similar scales,
+# standardization may be superfluous in this case.
+rides_standardized.describe().show()
+
+# Spark MLlib does not provide a transformer to unscale the features.  In order
+# to create meaningful plots below, we will proceed with unscaled features.
 
 
 # ## Specify and fit a k-means model
 
-# Specify and run the k-means algorithm
+# Use the `KMeans` class constructor to specify a k-means model:
 from pyspark.ml.clustering import KMeans
 kmeans = KMeans(featuresCol='features', predictionCol='cluster', k=3)
+type(kmeans)
 
 # Use the `explainParams` method to get a full list of the arguments:
 print(kmeans.explainParams())
 
 # Use the `fit` method to fit the k-means model:
 kmeans_model = kmeans.fit(rides_standardized)
+type(kmeans_model)
 
 # **Note:** Euclidean distance may not be appropriate in this case.
 
 
-# ## Evaluate the k-mean model
+# ## Evaluate the k-means model
 
 # Compute cluster costs:
 kmeans_model.computeCost(rides_standardized)
@@ -62,37 +80,35 @@ kmeans_model.clusterCenters()
 if kmeans_model.hasSummary:
   
   # Print cluster sizes
-  kmeans_model.summary.clusterSizes
+  print(kmeans_model.summary.clusterSizes)
   
   # Show predicitions
-  kmeans_model.summary.predictions.show()
+  kmeans_model.summary.predictions.show(5)
   
 # Plot cluster centers
-!pip install gmplot
-import gmplot
-from IPython.display import HTML
-lat, lon = gmplot.GoogleMapPlotter.geocode('Fargo, ND')
-mymap = gmplot.GoogleMapPlotter(lat, lon, 10)
+center_map = folium.Map(location=[46.8772222, -96.7894444])
 for cluster in kmeans_model.clusterCenters():
   # Plot marker at origin.
-  mymap.marker(cluster[0], cluster[1], color='green')
+  folium.Marker([cluster[0], cluster[1]], icon=folium.Icon(color="green")).add_to(center_map)
   # Plot marker at destination.
-  mymap.marker(cluster[2], cluster[3], color='red')
+  folium.Marker([cluster[2], cluster[3]], icon=folium.Icon(color="red")).add_to(center_map)
   # Plot line between origin and destination.
-  mymap.plot([cluster[0], cluster[2]], [cluster[1], cluster[3]])
-# Save the file in /cdn, which is required for CDSW.
-mymap.draw('/cdn/mymap.html')  # The file must be stored in /cdn.
-# Display the source file located in /cdn.
-HTML("<iframe src=mymap.html height='500px' width='800px'></iframe>")
-
-# **Note:** The markers are not displaying, most likely due to the fact that the 
-# the marker image is stored in /home/cdsw rather than /cdn.
+  folium.PolyLine([[cluster[0], cluster[1]], [cluster[2], cluster[3]]]).add_to(center_map)
+center_map
 
 
 # ## Explore cluster profiles
 
+# Get original DataFrame with cluster ids:
+predictions = kmeans_model.summary.predictions
+
+from pyspark.sql.functions import count, mean, stddev
+#predictions.groupBy("cluster").agg(count("distance"), mean("distance"), stddev("distance")).show()
+
 
 # ## Exercises
+
+# None
 
 
 # ## Cleanup
@@ -103,3 +119,4 @@ spark.stop()
 
 # ## References
 
+# [Clustering](http://spark.apache.org/docs/latest/ml-clustering.html)
---
+++ code-python/25_lda.py
@@ -0,0 +1,102 @@
+# # Topic Modeling with Latent Dirichlet Allocation (LDA)
+
+# In this module we use latent Dirichlet allocation (LDA) to look for topics
+# in the ride reviews.
+
+
+# ## Setup
+
+# Import useful packages, modules, classes, and functions:
+from __future__ import print_function
+from pyspark.sql import SparkSession
+
+# Create a SparkSession:
+spark = SparkSession.builder.master("local").appName("lda").getOrCreate()
+
+# Read the processed reviews data:
+reviews = spark.read.parquet("myduocar/clustering_data")
+
+# **Note:** This data is produced by the `20_extract.py` script.
+
+
+# ## Specify and fit an LDA model
+
+# Specify an LDA model:
+from pyspark.ml.clustering import LDA
+lda = LDA(featuresCol="words_vectorized", k=2)
+type(lda)
+
+# Print the full set of parameters:
+print(lda.explainParams())
+
+# Fit the LDA model:
+lda_model = lda.fit(reviews)
+type(lda_model)
+
+
+# ## Query the model
+
+lda_model.describeTopics().show(truncate=False)
+
+vocab = [u'driver',
+ u'ride',
+ u'air',
+ u'ride.',
+ u'car',
+ u'really',
+ u'freshener',
+ u'vehicle',
+ u'due',
+ u'freshener.']
+
+def f(x):
+  print([vocab[i] for i in x[1]])
+  
+lda_model.describeTopics().foreach(f)
+
+lda_model.estimatedDocConcentration()
+
+lda_model.logLikelihood(reviews)
+
+lda_model.logPerplexity(reviews)
+
+lda_model.topicsMatrix()
+
+reviews_with_topics = lda_model.transform(reviews)
+
+reviews_with_topics.head(5)
+
+lda_model.vocabSize()
+
+
+# ## Apply the LDS model
+
+reviews_with_topics = lda_model.transform(reviews)
+reviews_with_topics.head(5)
+
+
+# ## Exercises
+
+# None
+
+
+# ## Cleanup
+
+# Stop the SparkSession:
+spark.stop()
+
+
+# ## References
+
+# [Wikipedia - Latent Dirichlet allocation](https://en.wikipedia.org/wiki/Latent_Dirichlet_allocation)
+
+# [Spark Documentation - Latent Dirichlet allocation](http://spark.apache.org/docs/latest/ml-clustering.html#latent-dirichlet-allocation-lda)
+
+# [Spark Python API - LDA class](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.clustering.LDA)
+
+# [Spark Python API - LDAModel class](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.clustering.LDAModel)
+
+# [Spark Python API - LocalLDAModel class](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.clustering.LocalLDAModel)
+
+# [Spark Python API - DistributedLDAModel class](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.clustering.DistributedLDAModel)
+
