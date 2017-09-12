e3d574c8b41d83d875373c22efa7e22f2b4445fc
Brian Bloechle <bbloechle@cloudera.com>
2017-09-12 00:24:26 +0000

    Revise 01_connect_solutions.py

--- code-python/solutions/01_connect_solutions.py
+++ code-python/solutions/01_connect_solutions.py
@@ -1,43 +1,44 @@
 # # Connecting to Spark
 
-# Sample answers.  You can run this standalone, or execute selected statements in 
-# a wider session.
+# This script contains solutions to the exercises.  You can run this script in
+# a new session or execute selected statements in an existing session.  Note
+# that there is generally more than one way to solve a problem.
+
 
 # ## Exercises
 
-# Create a SparkSession that connects to Spark in local mode.  Configure the SparkSession to use two cores.  
-
+# (1) Create a SparkSession that connects to Spark in local mode.  Configure
+# the SparkSession to use two cores.  
 from pyspark.sql import SparkSession
-spark = SparkSession\
-  .builder\
-  .master("local")\
-  .appName("exercise_01")\
+spark = SparkSession \
+  .builder \
+  .master("local") \
+  .appName("connect_solutions") \
   .getOrCreate()
 
-
-# Create a small DataFrame.  
-# Print the schema.  View the DataFrame.  Count the number of records.
-
-person = spark\
-  .createDataFrame([(42, "Sarah", 11), (43, "Aaron", 11), (44, "Eddie", 12)],\
+# (2) Create a small DataFrame.
+person = spark \
+  .createDataFrame([(42, "Sarah", 11), (43, "Aaron", 11), (44, "Eddie", 12)], \
                    schema = ["id", "name", "city_id"])
 
+# Print the schema.
 person.printSchema()
 
+# View the DataFrame.
 person.show()
 
+# Count the number of records.
 person.count()
 
+# (3) Explore the Spark Job UI.
 
-# Explore the Spark Job UI.
+# * The Spark Job UI is accessible from the grid pulldown menu in the
+# upper-right of CDSW.
 
-
-# Stop the SparkSession.
-
+# (4) Stop the SparkSession.
 spark.stop()
 
-# Explore the Spark History Server UI.
+# (5) Explore the Spark History Server UI.
 
-# * (Viewable via the menu icon on the far right of the menu bar.)
-
-# ## Finis
+# * The Spark History Server UI is accessible from the grid pulldown menu in
+# the upper-right of CDSW.
