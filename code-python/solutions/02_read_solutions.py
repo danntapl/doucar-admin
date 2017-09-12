3dac4fde5aafb6b550e947967e3782fd7e76e7ea
Brian Bloechle <bbloechle@cloudera.com>
2017-09-12 18:04:29 +0000

    Revise 02_read_solutions.py - Add Hue exercise - Add notes

--- code-python/solutions/02_read_solutions.py
+++ code-python/solutions/02_read_solutions.py
@@ -1,49 +1,59 @@
 # # Reading and Writing Data
 
-# Sample answers.  You can run this standalone, or execute selected statements 
-# in a wider session.
+# This script contains sample solutions to the exercises.  You can run this
+# script in a new session or execute selected statements in an existing
+# session.  Note that there is generally more than one way to solve a problem.
 
 
 # ## Setup
 
-# Create SparkSession
+# Create a SparkSession:
 from pyspark.sql import SparkSession
-spark = SparkSession\
-  .builder\
-  .master("local")\
-  .appName("read-solutions")\
+spark = SparkSession \
+  .builder \
+  .master("local") \
+  .appName("read-solutions") \
   .getOrCreate()
 
 
 # ## Exercises
 
-# Read the raw driver file into a Spark DataFrame.
+# (1) Read the raw driver file from HDFS into a Spark DataFrame.
 
 drivers = spark.read.csv("/duocar/raw/drivers/", header=True, inferSchema=True)
 
 drivers.first()
 
-# Save the driver DataFrame as a JSON file in your CDSW practice directory.
+# (2) Save the driver DataFrame as a JSON file in your CDSW practice directory.
 
 !hdfs dfs -mkdir practice
+
 drivers.write.format("json").save("practice/drivers_json")
 
+# (3) Use Hue to inspect the JSON file.
 
-# Read the driver JSON file into a Spark DataFrame.
+# **Note:** The JSON file is noticeably larger than the corresponding CSV file.
+# Each row is a JSON object.
 
-drivers_from_json = spark\
-  .read\
-  .format("json")\
+# (4) Read the driver JSON file into a Spark DataFrame.
+
+drivers_from_json = spark \
+  .read \
+  .format("json") \
   .load("practice/drivers_json")
   
 drivers_from_json.first()
 
-  
-# ## Cleanup
+# **Note:** The columns are in alphabetical order.
 
-# Clear stored data from practice directory
+# (5) Delete the JSON file.
+  
 !hdfs dfs -rm -r -skipTrash practice/drivers_json
 
-# Stop session
+
+# ## Cleanup
+
+# Stop the SparkSession:
+
 spark.stop()
 
