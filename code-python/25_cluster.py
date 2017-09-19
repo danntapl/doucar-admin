822f36555fa52ad4c8946d6d85d7a70090ee484e
Brian Bloechle <bbloechle@cloudera.com>
2017-09-19 00:29:42 +0000

    Update clustering section - Move 25_cluster.py to appendices/25_kmeans.py - Add Gaussian mixture model example to 25_cluster.py - Move old LDA example to explore/25_lda.py - Move new LDA example to appendices/25_lda.py

--- code-python/25_cluster.py
+++ code-python/25_cluster.py
@@ -4,8 +4,8 @@
 # Not to be reproduced or shared without prior written 
 # consent from Cloudera.
 
-# In this module we use the k-means clustering functionality in Spark MLlib to
-# look for structure in the ride data.
+# In this module we use Gaussian mixture models in Spark MLlib to look for
+# structure in the rider data.
 
 
 # ## Setup
@@ -17,144 +17,115 @@
 # Import useful packages, modules, classes, and functions:
 from __future__ import print_function
 import folium
-import pandas as pd
 
 # Create a SparkSession:
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.master("local").appName("cluster").getOrCreate()
 
-# Load the enhanced ride data from HDFS:
-rides = spark.read.parquet("/duocar/joined/")
+# Load clean rider data from HDFS:
+riders = spark.read.parquet("/duocar/clean/riders/")
 
 
 # ## Preprocess the data
 
-# Remove the canceled rides:
-rides_filtered = rides.filter(rides.cancelled == 0)
+# Filter on student riders:
+from pyspark.sql.functions import col
+students = riders.filter(col("student") == True)
 
 
 # ## Extract, transform, and select the features
 
-# Let us try to cluster rides by origin and destination coordinates:
-features_selected = ["origin_lat", "origin_lon", "dest_lat", "dest_lon"]
+# Select features to cluster on home latitude and longitude:
+selected = ["home_lat", "home_lon"]
 
 # Assemble the feature vector:
 from pyspark.ml.feature import VectorAssembler
-va = VectorAssembler(inputCols=features_selected, outputCol="features")
-rides_assembled = va.transform(rides_filtered)
-
-# Standardizing the feature vector ensures that the features are in the same
-# scale and each feature is given a fair chance to contribute to the clustering
-# model:
-from pyspark.ml.feature import StandardScaler
-ss = StandardScaler(inputCol="features", outputCol="features_scaled", withMean=False, withStd=True)
-rides_standardized = ss.fit(rides_assembled).transform(rides_assembled)
-
-# **Note:** We are running the `StandardScaler` after the `VectorAssembler`.
-# This is more efficient in this example since `StandardScaler` will
-# standardize the entire feature vector in one operation.
-
-# **Note:** Given that latitude and longitude are in similar scales,
-# standardization may be superfluous in this case.
-rides_standardized.describe("origin_lat", "origin_lon", "dest_lat", "dest_lon").show()
-
-# Spark MLlib does not provide a transformer to unscale the features.  In order
-# to create meaningful plots below, we will proceed with unscaled features.
+assembler = VectorAssembler(inputCols=selected, outputCol="features")
+assembled = assembler.transform(students)
 
 
-# ## Specify and fit a k-means model
+# ## Build and evaluate a Gaussian mixture model
 
-# Use the `KMeans` class constructor to specify a k-means model:
-from pyspark.ml.clustering import KMeans
-kmeans = KMeans(featuresCol="features", predictionCol="cluster", k=3)
-type(kmeans)
+# Specify a Gaussian mixture model with two clusters:
+from pyspark.ml.clustering import GaussianMixture
+gm = GaussianMixture(featuresCol="features", k=2)
+type(gm)
 
-# Use the `explainParams` method to get a full list of the arguments:
-print(kmeans.explainParams())
+# Examine all arguments:
+print(gm.explainParams())
 
-# Use the `fit` method to fit the k-means model:
-kmeans_model = kmeans.fit(rides_standardized)
-type(kmeans_model)
+# Fit the Gaussian mixture model: 
+gm_model = gm.fit(assembled)
+type(gm_model)
 
-# **Note:** Euclidean distance may not be appropriate in this case.
+# Examine (multivariate) Gaussian distribution function:
+gm_model.gaussiansDF.head(5)
 
-
-# ## Evaluate the k-means model
-
-# Compute cluster costs:
-kmeans_model.computeCost(rides_standardized)
-
-# **Note:** The `computeCost` is generally not informative on its own.  It is
-# more useful when comparing multiple clustering models.
-
-# Print out the cluster centroids:
-kmeans_model.clusterCenters()
-
-if kmeans_model.hasSummary:
-  
-  # Print cluster sizes:
-  print(kmeans_model.summary.clusterSizes)
-  
-  # Show predictions:
-  kmeans_model.summary.predictions.printSchema()
-  kmeans_model.summary.predictions.select("features", "cluster").show(5, truncate=False)
-  
-# Plot cluster centers:
-center_map = folium.Map(location=[46.8772222, -96.7894444])
-for cluster in kmeans_model.clusterCenters():
-  # Plot marker at origin.
-  folium.Marker([cluster[0], cluster[1]], icon=folium.Icon(color="green")).add_to(center_map)
-  # Plot marker at destination.
-  folium.Marker([cluster[2], cluster[3]], icon=folium.Icon(color="red")).add_to(center_map)
-  # Plot line between origin and destination.
-  folium.PolyLine([[cluster[0], cluster[1]], [cluster[2], cluster[3]]]).add_to(center_map)
+# Plot Gaussian means (cluster centers):
+center_map = folium.Map(location=[46.8772222, -96.7894444], zoom_start=13)
+for cluster in gm_model.gaussiansDF.collect():
+  folium.Marker(cluster["mean"]).add_to(center_map)
 center_map
+
+# Examine mixing weights:
+gm_model.weights
+
+# Examine model summary:
+gm_model.hasSummary
+
+# Examine cluster sizes:
+gm_model.summary.clusterSizes
+
+# Examine predictions DataFrame:
+gm_model.summary.predictions.printSchema()
+gm_model.summary.predictions.select("prediction", "probability").head(5)
+
+
+# ## Save and apply clustering model
+
+# Remove model if it exists:
+!hdfs dfs -rm -r myduocar/gm_model
+
+# Save the Gaussian mixture model:
+gm_model.save("myduocar/gm_model")
+
+# Load the Gaussian mixture model:
+from pyspark.ml.clustering import GaussianMixtureModel
+gm_model_loaded = GaussianMixtureModel.load("myduocar/gm_model")
+
+# Apply the Gaussian mixture model:
+clustered = gm_model_loaded.transform(assembled)
+
+# Examine schema and view data:
+clustered.printSchema()
+clustered.head(5)
+
+# Compute cluster sizes:
+clustered.groupBy("prediction").count().orderBy("prediction").show()
 
 
 # ## Explore cluster profiles
 
-# Understanding a clustering model is an exercise in data analysis and
-# visualization.  We can bring our full toolkit to bear.
-
-# Get original DataFrame with cluster ids:
-predictions = kmeans_model.summary.predictions
-
-# Explore a categorical variable:
-predictions \
-  .crosstab("cluster", "service") \
-  .orderBy("cluster_service") \
+# Explore clusters:
+clustered \
+  .groupBy("prediction", "sex") \
+  .count() \
+  .orderBy("prediction", "sex") \
   .show()
 
-# Explore a continuous variable:
-from pyspark.sql.functions import count, mean, stddev
-predictions \
-  .groupBy("cluster") \
-  .agg(count("distance"), mean("distance"), stddev("distance")) \
-  .orderBy("cluster") \
-  .show()
-
-# Let us explore the cluster profiles more visually.
-
-# Create a random sample and read it into a pandas DataFrame:
-clustered = predictions.sample(withReplacement=False, fraction=0.01).toPandas()
-
-# Import the seaborn package:
+# Plot profiles:
+pandas_df = clustered.toPandas()
 import seaborn as sns
-
-# Plot the distribution of `service` for each cluster:
-sns.countplot(x="cluster", hue="service", data=clustered)
-
-# Plot the distribution of `distance` for each cluster:
-sns.boxplot(x="cluster", y="distance", data=clustered)
+sns.countplot("prediction", hue="sex", data=pandas_df)
 
 
 # ## Exercises
 
 # (1) Experiment with different values of k (number of clusters).
 
-# (2) Experiment with different sets of features.
+# (2) Experiment with other hyperparameters.
 
-# (3) Explore other clustering algorithms. 
+# (3) Look for clusters for all riders (not just student riders).
 
 
 # ## Cleanup
@@ -169,7 +140,9 @@ spark.stop()
 
 # [Spark Documentation - Clustering](http://spark.apache.org/docs/latest/ml-clustering.html)
 
-# [Spark Python API - KMeans class](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.clustering.KMeans)
+# [Spark Python API - GaussianMixture class](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.clustering.GaussianMixture)
 
-# [Spark Python API - KMeansModel class](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.clustering.KMeansModel)
+# [Spark Python API - GaussianMixtureModel class](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.clustering.GaussianMixtureModel)
+
+# [Spark Python API - GaussianMixtureSummary class](http://spark.apache.org/docs/latest/api/python/pyspark.ml.html#pyspark.ml.clustering.GaussianMixtureSummary)
 
